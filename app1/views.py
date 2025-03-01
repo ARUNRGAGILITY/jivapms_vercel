c6e4b0dcd515d9a00c9d40e8d7f95e3045593dbd
Arun R G <arun.agility@gmail.com>
2025-03-01 17:28:04 +0530

    revision

--- app1/views.py
+++ app1/views.py
@@ -6,5 +6,6 @@ def home(request):
 <h1>Welcome to My Django App on Vercel!</h1><br>Project/Product Management System. v1.
 <br>
 This is an open-source software based on MIT license.
+check1
 """
     return HttpResponse(f"{message}")
