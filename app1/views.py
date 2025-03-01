40be5e70e37a878593f199454313f222b31ec196
Arun R G <arun.agility@gmail.com>
2025-03-01 19:09:18 +0530

    revision

--- app1/views.py
+++ app1/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 app_name = "app1"
 mod_name = ""
 def home(request):
-    
+    message = "test"
     context = {
         'page': 'home',
         'page_title': 'Home Page'
