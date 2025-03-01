de76f801203104abd9ee65ef5e6274d7e191a5ea
Arun R G <arun.agility@gmail.com>
2025-03-01 19:47:14 +0530

    revision

--- project1/settings.py
+++ project1/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Debug Mode
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,15 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-import os
-
-# Base Directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Debug Mode
-DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 
