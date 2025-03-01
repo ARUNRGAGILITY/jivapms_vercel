69263a4e7395ecd92e602b6006c9f556e266174a
Arun R G <arun.agility@gmail.com>
2025-03-01 17:14:23 +0530

    revision

--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Welcome to My Django App on Vercel!</h1><br>Project/Product Management System")
+    return HttpResponse("<h1>Welcome to My Django App on Vercel!</h1><br>Project/Product Management System. v1")
